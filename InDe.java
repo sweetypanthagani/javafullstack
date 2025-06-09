fc05f3b440f74c2d194379aa44298cd718261419
panthagani sweety <sweetypanthagani71@gmail.com>
2025-06-09 16:09:21 +0530

    Add files via upload

---
+++ InDe.java
@@ -0,0 +1,14 @@
+class InDe
+{
+	public static void main(String args[])
+	 {
+    		int a = 5, b = 5;
+    		int x = ++a + ++a;  
+    		int y = a++  + a++;  
+		System.out.println(x);
+		System.out.println(y);
+	
+	
+	}
+
+}
