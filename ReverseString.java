98e645ec502adf70014db8a0e7b69a96c09f112a
panthagani sweety <sweetypanthagani71@gmail.com>
2025-06-12 09:50:40 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class ReverseString
+{
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter string ");
+		String s=sc.nextLine();
+		for(int i =s.length()-1;i>=0;i--)
+		{
+			char c=s.charAt(i);
+			System.out.print(c);
+		
+		
+		}
+		
+		
+		
+	
+	}
+}
